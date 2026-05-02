17675721a032fe4c478a41da9c100b7e60a41a9d
thanawatpanjayde123-bit <thanawatpanjayde123@gmail.com>
2026-05-03 01:25:41 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds]
+});
+
+client.once('ready', () => {
+  console.log(`✅ ${client.user.tag} ออนไลน์แล้ว`);
+});
+
+client.login(process.env.TOKEN);
