245db5557cdcf99c15c03f7b12d617d9fc13c6d3
thanawatpanjayde123-bit <thanawatpanjayde123@gmail.com>
2026-05-03 02:10:48 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 require('dotenv').config();
+
+const express = require("express");
+const app = express();
+
 const { Client, GatewayIntentBits } = require('discord.js');
 
 const client = new Client({
   intents: [GatewayIntentBits.Guilds]
 });
 
+// 🌐 เว็บไว้ให้ UptimeRobot ping
+app.get("/", (req, res) => {
+  res.send("Bot is alive!");
+});
+
+app.listen(3000, () => {
+  console.log("Web server ready.");
+});
+
+// 🤖 บอท
 client.once('ready', () => {
   console.log(`✅ ${client.user.tag} ออนไลน์แล้ว`);
 });
